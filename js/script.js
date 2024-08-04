5747533a53d0519c30a0567f6ccf1b2033c5575e
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-05 00:56:13 +0200

    js complete + add css, exercise complete

--- js/script.js
+++ js/script.js
@@ -20,18 +20,32 @@ indicati dall'utente
 Abbellire col CSS
 */
 
+// Recupero gli elementi dalla pagina html
+const myDiv = document.getElementById("my-div");
+const secondDiv = document.getElementById("second-div");
+
 // Ottengo il numero di km da percorrere e lo stampo
 const kmDaPercorrere = window.prompt("Quanti chilometri vuoi percorrere?", 10);
 console.log("Hai scelto di percorrere: " + kmDaPercorrere + "KM.");
 
 // Ottengo l'età del passeggero e la stampo
 const etaPasseggero = window.prompt("Quanti anni ha il passeggero?", 10);
-console.log("L'età selezionata per il passeggero è: " + etaPasseggero + ".");
+console.log(
+  "L'età selezionata per il passeggero è: " + etaPasseggero + " anni."
+);
 
 // Calcolo il prezzo del biglietto senza sconto in base al numero di
 // km da percorrere e lo stampo con 2 cifre decimali
 let costoBiglietto = kmDaPercorrere * 0.21;
 console.log("Il prezzo del biglietto è: " + costoBiglietto.toFixed(2) + ".");
+
+// Stampo il resoconto in pagina
+myDiv.innerText =
+  `Hai scelto di percorrere: ${kmDaPercorrere} KM.` +
+  `\n` +
+  `L'età selezionata per il passeggero è: ${etaPasseggero} anni.` +
+  `\n` +
+  `Il prezzo del biglietto è: ${costoBiglietto.toFixed(2)}€.`;
 
 // Prendendo in considerazione l'età del passeggero calcolo l'eventuale
 // sconto da applicare e stampo il prezzo con 2 cifre decimali
@@ -39,11 +53,17 @@ console.log("Il prezzo del biglietto è: " + costoBiglietto.toFixed(2) + ".");
 if (etaPasseggero < 18) {
   costoBiglietto -= costoBiglietto * 0.2; // Applica uno sconto del 20%
   console.log(
-    "Il costo del biglietto scontato è di: " + costoBiglietto.toFixed(2) + "€"
+    "Il prezzo del biglietto scontato è di: " + costoBiglietto.toFixed(2) + "€"
   );
+  secondDiv.innerText = `Il prezzo del biglietto scontato è di: ${costoBiglietto.toFixed(
+    2
+  )}€`;
 } else if (etaPasseggero > 60) {
   costoBiglietto -= costoBiglietto * 0.4; // Applica uno sconto del 40%
   console.log(
-    "Il costo del biglietto scontato è di: " + costoBiglietto.toFixed(2) + "€"
+    "Il prezzo del biglietto scontato è di: " + costoBiglietto.toFixed(2) + "€"
   );
+  secondDiv.innerText = `Il prezzo del biglietto scontato è di: ${costoBiglietto.toFixed(
+    2
+  )}€`;
 }
